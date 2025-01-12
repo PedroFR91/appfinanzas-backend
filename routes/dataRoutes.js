45b7bf4feae8daea50761d8391dd910d47756017
PedroFR91 <pedromfr91@gmail.com>
2025-01-12 01:25:02 +0100

    remove isauthenticated middleware from create data

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { createEntries } = require('../controllers/dataController');
-const isAuthenticated = require('../middlewares/isAuthenticated');
+
 
 // Ruta para crear entradas
-router.post('/', isAuthenticated, createEntries);
+router.post('/', createEntries);
 
 module.exports = router;
